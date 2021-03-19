fcbb358450121bd0ee6bcfd058256dd7ad0c694a
mdabdullaalnoman <noman.apparel.4003@gmail.com>
2021-03-19 16:35:09 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
